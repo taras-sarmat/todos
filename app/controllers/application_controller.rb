f79e29001ad0473ae5e73b2ba95ffdffbea1daba
taras_sarmat <taras_sarmat@ukr.net>
2018-05-18 11:14:18 +0300

    Refactoring Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,14 +8,14 @@ class ApplicationController < ActionController::Base
   end
 
   def sign_in?
-  	current_email.present?
+  	current_user.sign_in?
   end
-
-  def current_email
-  	session[:current_email]
-  end
-
+  
   def sign_in_as(email)
     session[:current_email] = email
+  end
+
+  def current_user
+    User.new(session[:current_email])
   end
 end
